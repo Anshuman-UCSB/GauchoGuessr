f3a5b8cc30aab779a88b26321cd8f530204071ab
MrJacobEisner <mrjeisner@gmail.com>
2024-01-13 11:52:32 -0800

    Add leaderboard styles and StrokeText component

--- frontend/src/components/leaderboard/Leaderboard.tsx
+++ frontend/src/components/leaderboard/Leaderboard.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import StrokeText from "../stroketext/StrokeText";
+import "./Leaderboard.scss";
 
 export default function Leaderboard() {
-    return <div>Leaderboard</div>;
+    return (
+        <div>
+            <StrokeText
+                text="LEADERBOARD"
+                fontFamily="'Inter', sans-serif"
+                color="#fff"
+                fontSize="40px"
+                fontStyle="italic"
+                fontWeight="900"
+                lineHeight="50px"
+                textAlign="center"
+                shadowColor="#000"
+                xOffset="-2px"
+                yOffset="2px"
+                webkitTextStroke="5px black"
+            />
+        </div>
+    );
 }
