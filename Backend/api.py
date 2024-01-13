a54e6e9754e39d56aed8dccf7a9522f45dc28b31
Anshuman Dash <biggergig@vps-6dfbd1aa.vps.ovh.us>
2024-01-13 07:57:11 +0000

    working leaderboard

--- Backend/api.py
+++ Backend/api.py
@@ -2,51 +2,56 @@ import firebase_admin
 from firebase_admin import firestore
 import threading
 from google.cloud.firestore_v1.base_query import FieldFilter
-
+import time
 # Application Default credentials are automatically created.
-app = firebase_admin.initialize_app()
-db = firestore.client()
 
 class Api:
 	def __init__(self):
-		...
+		try:
+			firebase_admin.get_app()
+		except ValueError:
+			firebase_admin.initialize_app()
+		self.db = firestore.client()
+		self.data = {}
+		print("logged in fine")
 
+	def registerLeaderboard(self, name):
+		if name not in ("default",):
+			print("Invalid leaderboard",name)
+			return False
+		if name in self.data:
+			print("Already registered",name)
+			return False
+		doc_ref = self.db.collection("leaderboards").document(name)
+		lb_col = doc_ref.collection("leaderboard")
 
-print("logged in fine", db)
-doc_ref = db.collection("leaderboards").document("default")
-default_collection = doc_ref.collection("leaderboard")
-default_collection.document("2").set({"name":"john","score":410,"time":firestore.SERVER_TIMESTAMP})
+		# default_collection.document("2").set({"name":"john","score":410,"time":firestore.SERVER_TIMESTAMP})
+		query = (
+			lb_col
+			.order_by('score', direction=firestore.Query.DESCENDING)
+			.limit(10)
+		)
 
-# Create an Event for notifying main thread.
-callback_done = threading.Event()
+		def on_snapshot(col_snapshot, changes, read_time):
+			print("Callback received query snapshot.")
+			self.data[name]=[v.to_dict() for v in col_snapshot]
+			self.data[name].sort(key=lambda x: -x['score'])
+			callback_done.set()
 
-# Create a callback on_snapshot function to capture changes
-def on_snapshot(doc_snapshot, changes, read_time):
-    for doc in doc_snapshot:
-        print(f"Received document snapshot: {doc.id}")
-    callback_done.set()
+		callback_done = threading.Event()
+		query_watch = query.on_snapshot(on_snapshot)
+		callback_done.wait()
+		return True
 
-# Watch the document
-doc_watch = doc_ref.on_snapshot(on_snapshot)
-
-query = (
-	default_collection
-	.order_by('score', direction=firestore.Query.DESCENDING)
-	.limit(10)
-)
-
-# Create an Event for notifying main thread.
-callback_done = threading.Event()
-
-# Create a callback on_snapshot function to capture changes
-def on_snapshot(col_snapshot, changes, read_time):
-    print("Callback received query snapshot.")
-    for doc in col_snapshot:
-        print(f"{doc.id}")
-    callback_done.set()
-
-# Watch the collection query
-query_watch = query.on_snapshot(on_snapshot)
-while True:
-	...
-# print([d.to_dict() for d in docs])
+	def getLeaderboard(self, name):
+		if name not in self.data and self.registerLeaderboard(name) == False:
+			raise Exception("Trying to read from bad leaderboard "+name)
+			return None
+		return self.data[name]
+	
+	def setScore(self, leaderboard, name, score):
+		lb_col = self.db.collection("leaderboards").document(name).collection("leaderboard")
+		lb_col.add({"name":name, "score":score, "time":firestore.SERVER_TIMESTAMP})
+if __name__=="__main__":
+	a = Api()
+	print(a.getLeaderboard("default"))
