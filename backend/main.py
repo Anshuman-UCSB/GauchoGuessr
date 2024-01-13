5649f7af0a23712807d106b7ca6b04a3fc3a6da0
Anshuman Dash <anshuman@ucsb.edu>
2024-01-13 21:55:30 +0000

    image api

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,10 @@ from typing import Union
 from fastapi import FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 from api import Api
+from game import Game
 import json, typing
+import string
+from random import choices
 from starlette.responses import Response
 
 # Run with: uvicorn main:app --reload 
@@ -12,6 +15,8 @@ app = FastAPI()
 api = Api()
 print("registering api")
 api.registerLeaderboard("default")
+
+games = {}
 
 class PrettyJSONResponse(Response):
     media_type = "application/json"
@@ -29,13 +34,36 @@ class PrettyJSONResponse(Response):
 def index(res: Response):
     return "hello"
 
+@app.get("/users", status_code=200, response_class=PrettyJSONResponse)
+def getUsers(res: Response):
+    return {"users":api.getUsers()}
+
 @app.get("/leaderboard/{leaderboard_name}", status_code=200, response_class=PrettyJSONResponse)
-def getLeaderboard(leaderboard_name: str):
+def getLeaderboard(leaderboard_name: str,res: Response):
     return api.getLeaderboard(leaderboard_name)
     # return {i:v for i,v in enumerate(api.getLeaderboard(leaderboard_name))}
+
 @app.post("/leaderboard/{leaderboard_name}", status_code=200, response_class=PrettyJSONResponse)
-def setScore(leaderboard_name: str, name:str, score:int):
+def setScore(leaderboard_name: str, name:str, score:int,res: Response):
     return api.setScore(leaderboard_name, name, score)
+
+@app.put("/game/", status_code=201, response_class=PrettyJSONResponse)
+def createGame(res: Response):
+    gameid = "".join(choices(string.ascii_uppercase,k=4))
+    while gameid in games:
+        print("regenerating duplicate game id",gameid)
+        gameid = "".join(choices(string.ascii_uppercase,k=4))
+    games[gameid] = Game()
+    return {"gameid":gameid}
+
+@app.get("/game/{gameid}/", status_code=200, response_class=PrettyJSONResponse)
+def getLink(gameid: str, stage:int, res: Response):
+    if gameid not in games:
+        res.status_code = 404
+        return "Game doesn't exist"
+    else:
+        return {"link":games[gameid].getLink(stage)}
+
 
 if __name__ == "__main__":
     import uvicorn
