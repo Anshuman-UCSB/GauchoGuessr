da57966ab51059f2357baa60a5c570bf53181e4d
MrJacobEisner <mrjeisner@gmail.com>
2024-01-13 11:36:26 -0800

    Refactor homepage styles and add stroke text component

--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -1,13 +1,42 @@
 import React from "react";
 import Leaderboard from "../../components/leaderboard/Leaderboard";
+import StrokeText from "../../components/stroketext/StrokeText";
 import "./Homepage.scss";
 
 export default function Homepage() {
     return (
         <div className="homepage">
             <div className="start">
-                <h1>GUACHO GUESSR</h1>
-                <button>Start</button>
+                <StrokeText
+                    text="GAUCHO GUESSR"
+                    fontFamily="'Inter', sans-serif"
+                    color="#fff"
+                    fontSize="150px"
+                    fontStyle="italic"
+                    fontWeight="900"
+                    lineHeight="140px"
+                    textAlign="center"
+                    shadowColor="#000"
+                    xOffset="-4px"
+                    yOffset="4px"
+                    webkitTextStroke="10px black"
+                />
+                <button>
+                    <StrokeText
+                        text="START!"
+                        fontFamily="'Inter', sans-serif"
+                        color="#fff"
+                        fontSize="100px"
+                        fontStyle="italic"
+                        fontWeight="900"
+                        lineHeight="normal"
+                        textAlign="center"
+                        shadowColor="#000"
+                        xOffset="-4px"
+                        yOffset="4px"
+                        webkitTextStroke="5px black"
+                    />
+                </button>
                 <p>
                     How well do you know UCSB’s campus? Guess your location to
                     earn points, and see if you can top the leaderboard!
