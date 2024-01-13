62c46ebfe2fbc05a6c1597e0464b5f74ef95a0fd
James Ellenstein <jamesellenstein@ucsb.edu>
2024-01-13 12:12:44 -0800

    Created router and linked gamepage to homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,11 +2,16 @@
 import React from "react";
 import "./App.scss";
 import Homepage from "./pages/homepage/Homepage";
+import { Routes, Route, Outlet, Link } from "react-router-dom";
+import Gamepage from "./pages/Gamepage";
 
 function App() {
     return (
         <>
-            <Homepage></Homepage>
+            <Routes>
+                <Route path="/" element={<Homepage />}/>
+                <Route path="/game" element={<Gamepage/>} />
+            </Routes>
         </>
     );
 }
--- frontend/src/pages/homepage/Homepage.tsx
+++ frontend/src/pages/homepage/Homepage.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import Leaderboard from "../../components/leaderboard/Leaderboard";
 import "./Homepage.scss";
+import {Link} from "react-router-dom";
 
 export default function Homepage() {
     return (
         <div className="homepage">
             <div className="start">
                 <h1>GUACHO GUESSR</h1>
-                <button>Start</button>
+                <Link to="/game">
+                    <button>Start</button>
+                </Link>
                 <p>
                     How well do you know UCSB’s campus? Guess your location to
                     earn points, and see if you can top the leaderboard!
