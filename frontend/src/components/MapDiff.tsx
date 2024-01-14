716b127c8747e2c5c8914be9d25095422f327238
James Ellenstein <jamesellenstein@ucsb.edu>
2024-01-13 16:52:59 -0800

    mostly done with Google maps integration

--- frontend/src/components/MapDiff.tsx
+++ frontend/src/components/MapDiff.tsx
@@ -1,41 +1,16 @@
 import React from 'react'
-import { GoogleMap, useJsApiLoader, Marker } from '@react-google-maps/api';
+import { GoogleMap, useJsApiLoader, Marker, Polyline } from '@react-google-maps/api';
 import { GOOGLE_MAPS_API_KEY } from '../config';
 
 const containerStyle = {
-  width: '400px',
-  height: '400px'
+  width: '800px',
+  height: '800px'
 };
 
-const SbBounds = {
-    
-    se: {
-        lat: 34.40421,
-        lng: -119.844215
-    },
-    ne: {
-        lat: 34.418661,
-        lng: -119.838186
-    },
-    sw: {
-        lat: 34.405928,
-        lng: -119.877925
-    },
-    west: {
-        lat: 34.419334,
-        lng: -119.873250
-    }
-}
-
-const center = {
-    //Coordinates of Pardall Tunnel
-  lat: 34.413103,
-  lng: -119.853269
-};
 const options = {
     streetViewControl: false,
     mapTypeControl: false,
-    zoom: 18,
+    zoom: 20,
     styles: [
         {
           featureType: 'poi',
@@ -57,29 +32,31 @@ const options = {
       ],
   };
 
-function MyMap() {
+interface MapProps {
+  UserMarker: {
+    lat: number;
+    lng: number;
+  };
+  realMarker: {
+    lat: number;
+    lng: number;
+  };
+  distance: number;
+  }
+
+function DiffMap(MapProps: MapProps) {
   const { isLoaded } = useJsApiLoader({
     id: 'google-map-script',
     googleMapsApiKey: GOOGLE_MAPS_API_KEY
   })
-  const points = [SbBounds.se, SbBounds.ne, SbBounds.sw, SbBounds.west]
   const [map, setMap] = React.useState(null)
-  const [marker, setMarker] = React.useState(center)
-  const onMapClick = (e:any) => {
-    setMarker(
-      {
-        lat: e.latLng.lat(),
-        lng: e.latLng.lng()
-      }
-    );
-  };
-  const onLoad = React.useCallback(function callback(map:any    ) {
+  const onLoad = React.useCallback(function callback(map:any) {
     const bounds = new window.google.maps.LatLngBounds();
     points.forEach((point) => {
       bounds.extend(new window.google.maps.LatLng(point.lat, point.lng));
     });
     map.fitBounds(bounds);
-
+  
 
     setMap(map)
   }, [])
@@ -87,24 +64,39 @@ function MyMap() {
   const onUnmount = React.useCallback(function callback(map:any) {
     setMap(null)
   }, [])
-
+  const points = [MapProps.realMarker, MapProps.UserMarker];
   return isLoaded ? (
       <GoogleMap
         mapContainerStyle={containerStyle}
-        center={center}
-        zoom={10}
+        center={MapProps.realMarker}
+        zoom={20}
         onLoad={onLoad}
         onUnmount={onUnmount}
-        onClick={onMapClick}
         streetView={undefined}
         options={options}
       >
-        <Marker position={marker}> </Marker>
+        <Marker position={MapProps.realMarker} label= {"Actual Location: Lat " + MapProps.realMarker.lat + " Long " + MapProps.realMarker.lng}>Hello World </Marker>
+        <Marker position={MapProps.UserMarker} label= {"Your Location: Lat " + MapProps.UserMarker.lat + " Long " + MapProps.UserMarker.lng}> HI again</Marker>
+        {/* {points.map((point, index) => (
+        <Marker
+          key={index}
+          position={{ lat: point.lat, lng: point.lng }}
+        />
+      ))} */}
+
+      <Polyline
+        path={points.map((point) => ({ lat: point.lat, lng: point.lng }))}
+        options={{
+          strokeColor: '#FF0000',
+          strokeOpacity: 1,
+          strokeWeight: 2,
+        }}
+      />
         <></>
       </GoogleMap>
   ) : <></>
 }
 
-export default React.memo(MyMap)
+export default React.memo(DiffMap)
 
 //export default <div></div>
