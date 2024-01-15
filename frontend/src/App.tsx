a49d8a92d4e2ff517550067ca5fc57a1e2a4c049
MrJacobEisner <mrjeisner@gmail.com>
2024-01-15 15:52:39 -0800

    Add Firebase SDK to package.json and initialize Firebase in App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,26 @@ import "./App.scss";
 import Homepage from "./pages/homepage/Homepage";
 import Gamepage from "./pages/gamepage/Gamepage";
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyD2xW0cH_eqGiTUWzttBPQY_Kgk175017o",
+    authDomain: "gauchoguessr-ee340.firebaseapp.com",
+    projectId: "gauchoguessr-ee340",
+    storageBucket: "gauchoguessr-ee340.appspot.com",
+    messagingSenderId: "218457441803",
+    appId: "1:218457441803:web:56aacaa10ccc00d2a5290e",
+    measurementId: "G-82Q1BD6MRS",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
 function App() {
     const [state, setState] = useState(1);
 
